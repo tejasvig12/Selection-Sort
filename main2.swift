09b1b64495c8365de07692d365370eda1fc44e1f
tejasvig12 <tejasvi.gutta@student.allenisd.org>
2022-10-13 10:05:01 -0500

    new file:   main2.swift 	new file:   ordered-10e1.txt 	new file:   ordered-10e2.txt 	new file:   ordered-10e3.txt 	new file:   ordered-10e4.txt 	new file:   ordered-10e5.txt 	new file:   ordered.txt 	new file:   random-10e1.txt 	new file:   random-10e2.txt 	new file:   random-10e3.txt 	new file:   random-10e4.txt 	new file:   random-10e5.txt 	new file:   reverse-ordered-10e1.txt 	new file:   reverse-ordered-10e2.txt 	new file:   reverse-ordered-10e3.txt 	new file:   reverse-ordered-10e4.txt 	new file:   reverse-ordered-10e5.txt 	new file:   reverse-ordered.txt

---
+++ main2.swift
@@ -0,0 +1,49 @@
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ���  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩   // ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [5, 1, 4, 2, 8]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+var array = unsortedIntegers
+print("Pass: 0, Swaps: 0/0, Array: \(array)")
+var small = array[0]
+var temp = 0
+var index = 0
+var finished = -100000000000000
+var total = array.count
+
+var greatest = array[0]
+for num in array {
+    if num > greatest {
+        greatest = num
+    }
+}
+for _ in 0 ..< total-1 {
+    index = 0
+    small = greatest
+    for num in array {
+        if num < small && num > finished {
+            small = num
+        }
+    }
+    finished = small
+    while array[index] != small {
+        index += 1
+    }
+    var x = array[index]
+    var y = array[temp]
+    let z = array[index]
+    x = y
+    y = z
+    array[index] = x
+    array[temp] = y
+    temp += 1
+
+    print("Pass: \(temp), Swaps: 1/\(temp), Array: \(array)")
+}
+
+                                                                                                       
